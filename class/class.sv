e005a5a2d6c6a156decb506575cdd6c19d5e5b60
nitish <nitishyadav@mirafra.com>
2022-09-13 12:04:59 +0530

    updated

--- class/class.sv
+++ class/class.sv
@@ -1,24 +1,27 @@
 class home;   //define class  
 
-//declare class properties  
+
+ //declare class properties  
   
     bit light;  
     int fan;  
-    logic door;    
-   
-//define class method(task/function)  
+    string switch;  
+
+ //define class method(task/function)  
   
-  task open_electricity();  
-     light=1;  
-     fan=1;
-     $display("electricity is open");
-    endtask  
-   endclass  
+   task open_electricity();  
+     switch = "ON";  
+     $display("switch is %s so electricity is open",switch);
+   endtask  
+endclass
 
   module check_electricity;  
    initial begin  
-     home home_1;   //creating handle  
-     home_1=new();   //Creating Object for the Handle  
-     home_1.open_electricity;
+     home home_1;             //creating handle  
+     home_1=new();            //Creating Object for the Handle 
+     home_1.fan=1;            // access the properties
+     home_1.light=1;
+     home_1.open_electricity;          // access the method
+      $display("light=%0d fan=%0d",home_1.light,home_1.fan);
    end  
   endmodule
