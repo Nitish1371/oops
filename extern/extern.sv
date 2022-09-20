783de3009102e8481e338cdb1cf3a8af9b6f20ec
nitish <nitishyadav@mirafra.com>
2022-09-20 10:00:47 +0530

    updated

--- extern/extern.sv
+++ extern/extern.sv
@@ -1,15 +1,19 @@
 //-------------------------------------------------------
-//
-//
+//using the extern keyword we define the class method outside 
+//the body of class
 //-------------------------------------------------------
 class home;
   string switch;
-  string fan = "ON";
+  string fan = "OFF";
+
+  //function declaration - extern indicates out-of-body declaration
   extern function void display();
 endclass
+
+  //function implementation outside class body
 function void home::display();
-  string switch="ON";
-  $display("Thw switch is %0s that's why fan is %0s",fan,switch);
+  string switch="OFF";
+  $display("The switch is %0s that's why fan is %0s",fan,switch);
 endfunction
 
 module extern_example;
--- super/super.sv
+++ super/super.sv
@@ -1,11 +1,12 @@
 //-------------------------------------------------------
-//
-//
+//If the method of the parent class is overridden in the child 
+ //class, then using the ‘super’ keyword parent class method
+//can be accessed from the child class.
 //-------------------------------------------------------
 class base_class;
-  string fan,switch; //
+  string fan,switch; //properties of class
 
-  function void display();
+  function void display();    //method of class
     switch="ON";
     $write("switch is %s " ,switch);
   endfunction
@@ -14,17 +15,17 @@ endclass
 class sub_class extends base_class;
  string fan="ON";
   function void display();
-    super.display;
+    super.display; //define the super keyword inside the sub_class
     $write("that's why fan is %s \n" ,fan);
   endfunction 
 endclass
 
-sub_class s1;
+sub_class s1;  //creating handle for class
 
 module super_example;
 initial begin
-  s1 =new();
-  s1.display();
+  s1 =new();    //create an object
+  s1.display();  //access the sub_class method
 end
 endmodule
 
